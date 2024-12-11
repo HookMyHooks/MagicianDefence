5b98e0fdb2351dcad71a15603a3251c51d894753
HookMyHooks <liviunedelcu10@gmail.com>
2024-12-11 20:33:45 +0200

    Almost fixed animations

--- MagicianDefense/Assets/Scripts/PlayerMovement.cs
+++ MagicianDefense/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _MovementSpeed;
     [SerializeField] private float _RotationSpeed;
     [SerializeField] private float _JumpSpeed;
+    [SerializeField] private Animator _animator;
+
     void Start()
     {
+        _animator = GetComponent<Animator>();   
         _rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -17,22 +21,35 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += transform.forward * _MovementSpeed * Time.deltaTime;
+            _animator.SetInteger("AnimState", 1);
+            Debug.Log(_animator.GetInteger("AnimState"));
+
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += -transform.forward * _MovementSpeed * Time.deltaTime;
+            //will be another value for a back running animation
+            _animator.SetInteger("AnimState", 1);
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.eulerAngles += Vector3.down * _RotationSpeed * Time.deltaTime;
+            _animator.SetInteger("AnimState", 2);
+
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.eulerAngles += Vector3.up * _RotationSpeed * Time.deltaTime;
+            _animator.SetInteger("AnimState", 3);
+
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody.AddForce(new Vector3(0, _JumpSpeed), ForceMode.Impulse);
         }
+
+        _animator.SetInteger("AnimState", 0);
+
     }
 }
